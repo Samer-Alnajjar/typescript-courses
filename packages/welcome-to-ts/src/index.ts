8a66f1e6d1e386716969a2b8ffd263d3355dbb69
Samer Alnajjar <44429186+Samer-Alnajjar@users.noreply.github.com>
2026-04-23 14:22:12 +0300

    Intro

--- packages/welcome-to-ts/src/index.ts
+++ packages/welcome-to-ts/src/index.ts
@@ -27,3 +27,36 @@ class Foo {
 (async () => {
   console.log(await addNumbers(Foo.getValue(), 4));
 })();
+
+
+class Car {
+  make: string
+  model: string
+  year: number
+  isElectric: boolean
+}
+ 
+class Truck {
+  make: string
+  model: string
+  year: number
+  towingCapacity: number
+}
+ 
+const vehicle = {
+  make: "Honda",
+  model: "Accord",
+  year: 2017,
+}
+ 
+function printCar(car: {
+  make: string
+  model: string
+  year: number
+}) {
+  console.log(`${car.make} ${car.model} (${car.year})`)
+}
+ 
+printCar(new Car()) // Fine
+printCar(new Truck()) // Fine
+printCar(vehicle) // Fine
